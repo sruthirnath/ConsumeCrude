ea88da0a3ad9490382a6a98e7d4b4ed28f7ebde3
Sruthi Nath <sruthyrnath@gmail.com>
2017-09-08 13:52:41 +0530

    ConsumeCrude

---
+++ ConsumeCrude/Controllers/api/SampleController.cs
@@ -0,0 +1,37 @@
+﻿using ConsumeCrude.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ConsumeCrude.Controllers.api
+{
+    public class SampleController : ApiController
+    {
+
+        [HttpPost]
+        [Route("create")]
+        public IHttpActionResult create(stud_Details s)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid student.");
+            using (var context = new StudentEntities())
+            {
+                context.stud_Details.Add(new stud_Details()
+                {
+                    ID = s.ID,
+                    Name = s.Name,
+                    Age = s.Age
+
+                });
+
+
+
+                context.SaveChanges();
+                return Ok("successfull");
+            }
+        }
+    }
+}
---
+++ ConsumeCrude/Controllers/mvc/SampleController.cs
@@ -0,0 +1,44 @@
+﻿using ConsumeCrude.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumeCrude.Controllers.mvc
+{
+    public class SampleController : Controller
+    {
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(stud_Details student)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:60188//api/");
+
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync("student", student);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            return View(student);
+        }
+
+
+    }
+}
